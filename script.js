513d54b028e9422513b3268b28462840a0051f29
Jordan <jordanlaxa22@gmail.com>
2023-03-06 16:27:00 -0800

    Add equal and clear buttons, display, and comments

---
+++ script.js
@@ -0,0 +1,15 @@
+var add = document.querySelector('#add');
+add.addEventListener('click', () => {
+operate(firstNumber="paper", secondNumber = getComputerChoice);
+});
+
+// 1a. create add function
+// 1b. create subtract
+// 1c. create multiply
+// 1d. create divide
+// 2a. create operate function, takes operator
+// and two numbers and does operation
+// 3a. create basic html calculator with buttons for
+// each digit, each function, and "equals" key
+// 3b. add display to calculator
+// 3c. add a "clear" button
